3e9b1dac4b29dc6c647e0309ec6bdc4924882721
b0rtvader <bort.dm@gmail.com>
2017-10-16 21:03:51 +0500

    Update lab2_pymystem.py

--- lab2_pymystem.py
+++ lab2_pymystem.py
@@ -1,11 +1,15 @@
 from pymystem3 import Mystem
 geoset=set()
 mystem = Mystem()
-with open("warANDpeaceSmall.txt", "rb") as f: 
+with open("warANDpeace.txt", "rb") as f: 
     sourceText = f.read().decode('utf8')   
-for token in mystem.analyze(sourceText):        
-    if('analysis' in token.keys()):
-        tokenAnalysis = token['analysis']
-        if(len(tokenAnalysis)>0) and ('гео' in tokenAnalysis[0]['gr']):
-            geoset.add(tokenAnalysis[0]['lex'])
-print(geoset)
+for token in mystem.analyze(sourceText): 
+    try:       
+        tokenAnalysis = token['analysis'][0]
+        if('гео' in tokenAnalysis['gr']):
+            geoset.add(tokenAnalysis['lex'])
+    except KeyError:        # Возникает в спецсимволах. (token['analysis'] отсутствует)
+    	pass
+    except IndexError:		# Возникает в словах на латинице. Разбор данных слов не проводится (token['analysis'][0] отсутствует)
+    	pass
+print(sorted(geoset))
