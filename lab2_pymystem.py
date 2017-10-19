ff25b38b640ed390dc7a6a27ec03650db4c076b5
b0rtvader <bort.dm@gmail.com>
2017-10-19 14:56:50 +0500

    Update lab2_pymystem.py

--- lab2_pymystem.py
+++ lab2_pymystem.py
@@ -1,11 +1,14 @@
 from pymystem3 import Mystem
 mystem = Mystem()
 with open("warANDpeace.txt", "rb") as f: 
-    sourceText = f.read().decode('utf8')   
+    sourceText = f.read().decode('utf8')  
+geoSet = set()
 for token in mystem.analyze(sourceText): 
     try:       
         for tokenAnalysis in token['analysis']:
             if('гео' in tokenAnalysis['gr']):
-                print(tokenAnalysis['lex'])
+                geoSet.add(tokenAnalysis['lex'])
     except KeyError:        # Возникает в спецсимволах. (token['analysis'] отсутствует)
     	pass
+for geo in sorted(geoSet):
+    print(geo)
